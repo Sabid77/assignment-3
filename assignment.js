a2c50a6c531ad56bb49b142401119c02670b8cbe
M sabid <sabidsabid77@gmail.com>
2021-02-04 23:49:36 +0600

    Work DOne

--- assignment.js
+++ assignment.js
@@ -1,15 +1,52 @@
-// var kilometter = 1;
-// var mitter = 1000;
-// var result = kilometter * mitter;
-// console.log(result);
-const bigName = (nameArray) =>{
-    const length = nameArray.map(function(word){
-    return word.length
-   })
+// Kilo to Meter
+function kilometerToMeter(kilo) {
+    var kilometers = kilo * 1000;
+    console.log(kilometers);
+}
+kilometerToMeter(1);
+// budgetCalculator
+function budgetCalculator(phone, clock, laptop) {
+    var phonePrice = phone * 200; 
+    var clockPrice = clock * 100;
+    var laptopPrice = laptop * 300;
+    var total = phonePrice + clockPrice + laptopPrice;
+    console.log(total);
+}
+budgetCalculator(2, 2, 2);
+// hotelCost
+function hotelCost(number) {
+
+    // var oneToTen = number * 100;
+    // var elevenToTwenty = number * 80;
+    // var more = number * 50; 
+
+    if (number <= 10) {
+        var oneToTen = number * 100;
+        console.log(oneToTen);
+    } 
+    else if (number <= 20) {
+        var extraDays = number - 10;
+        var elevenToTwenty = (10*100) + extraDays * 80;
+        console.log(elevenToTwenty);
+    }
+    else {
+        var extraDays = number - 20;
+        var moreThenTwenty = (10*100) + (10*80) + extraDays * 50;
+        console.log(moreThenTwenty);
+    }
+    
+    // var oneToTen = 11;
+    // var elevenToTwenty = number * 80;
+    // var more = number * 50; 
+}
+hotelCost(21);
+// Big Name Handler
+const bigName = (nameArray) => {
+    const length = nameArray.map(function (word) {
+        return word.length
+    })
     let i = length.indexOf(Math.max(...length));
     return nameArray[i];
-   }
-   
-   const nameArray = ["Rafiq","karim","ShobujAlom","Sabid","sumona","jhankarMahbub"];
-   
-   console.log(bigName(nameArray));
+}
+const nameArray = ["Rafiq", "karim", "ShobujAlom", "Sabid", "sumona", "Jhankar Mahbub"];
+console.log(bigName(nameArray));
